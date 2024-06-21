3c25a801fd911fb6268128312da380b4147c706e
Chandra Mahesh <chandachandramahesh@gmail.com>
2024-06-21 09:48:13 +0530

    self react practice

---
+++ App.js
@@ -0,0 +1,5 @@
+const header = React.createElement('h1', {id: 'heading'}, 'This is heading Tag');
+
+const head = ReactDom.createRoot(document.querySelector('root').innerHTML = header);
+
+root.render(head);
